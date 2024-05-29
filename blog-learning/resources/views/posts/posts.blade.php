904dfeddfaf50b1ec87410ad7dea72f376bd5565
Mohammad Beta <swiftbooksjob@hotmail.com>
2024-05-29 20:06:56 +0300

    add create new post button in posts view

--- blog-learning/resources/views/posts/posts.blade.php
+++ blog-learning/resources/views/posts/posts.blade.php
@@ -43,6 +43,7 @@
           </div>
         </div>
       </nav>
+
       <table class="table table-hover">
         <thead>
           <tr>
@@ -70,6 +71,11 @@
           
         </tbody>
       </table>
+      <div class="text-center">
+      <div class="mt-4">
+        <a  class="btn btn-success">Create New Post</a>
+      </div>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
